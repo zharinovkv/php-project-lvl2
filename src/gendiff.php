c2cd3e4cc37b7373b0b30efe01326a39d84247c2
Konstantin Zharinov <zharinovkv@gmail.com>
2019-09-04 21:04:50 +0500

    Init commit

---
+++ src/gendiff.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace php_project_lvl2\gendiff;
+
+use Docopt;
+
+function run()
+{
+
+$doc = <<<'DOCOPT'
+Generate diff
+
+Usage:
+  gendiff (-h|--help)
+  gendiff (-v|--version)
+
+  Options:
+  -h --help                     Show this screen
+  -v --version                  Show version
+
+DOCOPT;
+   
+    $result = Docopt::handle($doc, array('version'=>'1.0.0rc2'));
+
+    foreach ($result as $k=>$v) {
+        echo $k.': '.json_encode($v).PHP_EOL;
+    }
+}
