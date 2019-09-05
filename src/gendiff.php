c4e7b0fd71c78e064f86b11b3fd4ecea193213cf
Konstantin Zharinov <zharinovkv@gmail.com>
2019-09-05 21:10:16 +0500

    2й шаг

--- src/gendiff.php
+++ src/gendiff.php
@@ -6,23 +6,24 @@ use Docopt;
 
 function run()
 {
-
     $doc = <<<'DOCOPT'
 Generate diff
 
 Usage:
   gendiff (-h|--help)
   gendiff (-v|--version)
+  gendiff [--format <fmt>] <firstFile> <secondFile>
 
   Options:
   -h --help                     Show this screen
   -v --version                  Show version
-
+  --format <fmt>                Report format [default: pretty]
+  
 DOCOPT;
-   
+
     $result = Docopt::handle($doc, array('version' => '1.0.0rc2'));
 
-    foreach ($result as $k => $v) {
-        echo $k . ': ' . json_encode($v) . PHP_EOL;
+    foreach ($result as $key => $value) {
+        echo $key . ': ' . json_encode($value) . PHP_EOL;
     }
 }
