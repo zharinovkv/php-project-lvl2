45bccec5e745cc4517d3a228435eace0c9d1d466
Konstantin Zharinov <zharinovkv@gmail.com>
2020-04-10 22:49:20 +0500

    Выполнение требований третьего шага

--- src/differ.php
+++ src/differ.php
@@ -50,7 +50,7 @@ function parser($content)
         foreach ($beforeArr as $key_before => $value_before) {
             foreach ($afterArr as $key_after => $value_after) {
                 if ($key_before == $key_after && $value_before != $value_after) {
-                    $result .= '+ ' . $key_after . ': ' . $value_after . PHP_EOL . 
+                    $result .= '+ ' . $key_after . ': ' . $value_after . PHP_EOL .
                         '- ' . $key_before . ': ' . $value_before . PHP_EOL;
                     break;
                 }
