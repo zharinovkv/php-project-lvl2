5d96e3642a574171903911d0786894c54c3cdee4
Konstantin Zharinov <zharinovkv@gmail.com>
2020-04-19 18:44:08 +0500

    Сделано АСТ для плоских файлов

--- src/differ.php
+++ src/differ.php
@@ -2,66 +2,29 @@
 
 namespace Differ\differ;
 
-use function Differ\parsers\parser;
+use function Differ\parser\parser;
+use function Differ\parser\toAst;
+use function Differ\parser\render;
+use const Differ\parser\keys as keys;
 
 const ASSETS = 'assets/';
 
 function genDiff($path_before, $path_after)
 {
-    $paths = getAbsolutePathToFile($path_before, $path_after);
+    $paths = getPathsToFiles($path_before, $path_after);
+
     $content = parser($paths);
+    $ast = toAst($content);
+    $str = render($ast);
 
-    [$before, $after] = [getBeforeOrAfter($content, 'path_before'), getBeforeOrAfter($content, 'path_after')];
-
-    $repeatElements = toString(getElements(array_intersect($before, $after), ' '));
-    $revisedElements = revisedElements($before, $after);
-    $unicalElementsAfter = toString(getElements(array_diff_key($before, $after), '-'));
-    $unicalElementsBefore = toString(getElements(array_diff_key($after, $before), '+'));
-
-    return "{\n{$repeatElements}{$revisedElements}{$unicalElementsAfter}{$unicalElementsBefore}}\n";
+    return $str;
 }
 
-function getAbsolutePathToFile(...$path)
+function getPathsToFiles(...$paths)
 {
-    return [
-        'path_before' => ! (bool)substr_count($path[0], '/') ? $path[0] = ASSETS . $path[0] : $path[0],
-        'path_after' => ! (bool)substr_count($path[1], '/') ? $path[1] = ASSETS . $path[1] : $path[1]
-    ];
-}
-
-function getBeforeOrAfter($content, $index)
-{
-    return $content[$index];
-}
-
-function revisedElements($beforeArr, $afterArr)
-{
-    $result = '';
-
-    foreach ($beforeArr as $key_before => $value_before) {
-        foreach ($afterArr as $key_after => $value_after) {
-            if ($key_before == $key_after && $value_before != $value_after) {
-                $result .= '+ ' . $key_after . ': ' . $value_after . PHP_EOL . '- ' .
-                    $key_before . ': ' . $value_before . PHP_EOL;
-                break;
-            }
-        }
-    }
-    return $result;
-}
-
-function getElements($array, $prefix)
-{
-    $result = [];
-    foreach ($array as $key => $value) {
-        array_push($result, "{$prefix} {$key}: " . json_encode($value));
-    }
-    return $result;
-}
-
-function toString($array)
-{
-    $result = implode(PHP_EOL, $array);
-    $result = str_replace("\"", "", $result);
-    return $result . PHP_EOL;
+    $pathsToFiles = array_map(function ($path) {
+        return !(bool) substr_count($path, '/') ? $path = '{ASSETS}{$path}' : $path;
+    }, $paths);
+    //$keys = ['path_before', 'path_after'];
+    return array_combine(keys, $pathsToFiles);
 }
--- src/parser.php
+++ src/parser.php
@@ -1,20 +1,126 @@
 <?php
 
-namespace Differ\parsers;
+namespace Differ\parser;
 
 use Symfony\Component\Yaml\Yaml;
 
+const keys = ['path_before', 'path_after'];
+
 function parser($paths)
 {
-    $ext = pathinfo($paths['path_before'], PATHINFO_EXTENSION);
-
     $json = function ($path) {
         return json_decode(file_get_contents($path), true);
     };
 
     $yaml = function ($path) {
-        return Yaml::parseFile($path);
+        return Yaml::parseFile($path/* , Yaml::PARSE_OBJECT_FOR_MAP */);
     };
 
+    $ext = strtolower(pathinfo($paths[keys[0]], PATHINFO_EXTENSION));
+
     return array_map(${$ext}, $paths);
+}
+
+function toAst($content)
+{
+    [$before, $after] = [(array) getBeforeAfter($content, keys[0]), (array) getBeforeAfter($content, keys[1])];
+
+    $unchanged = unchanged($before, $after);
+    $changed =  changed($before, $after);
+    $removed =  removed($before, $after);
+    $added =  added($before, $after);
+
+    $result = array_merge_recursive($unchanged, $changed, $removed, $added);
+    return $result;
+}
+
+function unchanged($before, $after)
+{
+    $result = array_intersect($before, $after);
+
+    $unchanged = array_map(function ($key, $value) {
+        return ['type' => 'unchanged', 'name' => $key, 'oldValue' => $value, 'newValue' => $value];
+    }, array_keys($result), $result);
+
+    return $unchanged;
+};
+
+function changed($before, $after)
+{
+    $changedItems = function ($my_array, $allowed) {
+        $filtered = array_filter(
+            $my_array,
+            function ($val, $key) use ($allowed) {
+                return isset($allowed[$key]) && ($allowed[$key] === true || $allowed[$key] !== $val);
+            },
+            ARRAY_FILTER_USE_BOTH
+        );
+        return $filtered;
+    };
+
+    $filtered1 = $changedItems($before, $after);
+    $filtered2 = $changedItems($after, $before);
+    $merged = array_merge_recursive($filtered1, $filtered2);
+
+    $result2 = array_map(function ($key, $value) {
+        return ['type' => 'changed', 'name' => $key, 'oldValue' => $value[0], 'newValue' => $value[1]];
+    }, array_keys($merged), $merged);
+
+    return $result2;
+}
+
+function removed($before, $after)
+{
+    $result = array_diff_key($before, $after);
+
+    $result2 = array_map(function ($key, $value) {
+        return ['type' => 'removed', 'name' => $key, 'oldValue' => $value, 'newValue' => ''];
+    }, array_keys($result), $result);
+
+    return $result2;
+}
+
+function added($before, $after)
+{
+    $result = array_diff_key($after, $before);
+
+    $result2 = array_map(function ($key, $value) {
+        return ['type' => 'added', 'name' => $key, 'oldValue' => '', 'newValue' => json_encode($value)];
+    }, array_keys($result), $result);
+
+    return $result2;
+}
+
+function getBeforeAfter($content, $index)
+{
+    return $content[$index];
+}
+
+function render($ast)
+{
+    //var_dump($ast);
+
+    $types = [
+        'unchanged' => function ($value) {
+            return "  {$value['name']}: {$value['oldValue']}";
+        },
+        'changed' => function ($value) {
+            return "+ {$value['name']}: {$value['newValue']}\n- {$value['name']}: {$value['oldValue']}";
+        },
+        'removed' => function ($value) {
+            return "- {$value['name']}: {$value['oldValue']}";
+        },
+        'added' => function ($value) {
+            return "+ {$value['name']}: {$value['newValue']}";
+        },
+        'nested' => function ($value) {
+            return 'nested';
+        },
+    ];
+
+    $result = array_map(function ($value) use ($types) {
+        return $types[$value['type']]($value);
+    }, $ast);
+
+    return "{\n" . join("\n", $result) . "\n}\n";
 }
