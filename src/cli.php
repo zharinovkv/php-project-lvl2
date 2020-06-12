1f3981930789cf0a79c5071e61ca6545ecea0727
Konstantin Zharinov <zharinovkv@gmail.com>
2020-06-12 16:33:58 +0500

    Новое название файла

---
+++ src/cli.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Differ\differ;
+
+use Docopt;
+
+use function Differ\gendiff\genDiff;
+
+function run()
+{
+    $doc = <<<'DOCOPT'
+Generate diff
+
+Usage:
+  gendiff (-h|--help)
+  gendiff (-v|--version)
+  gendiff [--format <fmt>] <firstFile> <secondFile>
+
+  Options:
+  -h --help                     Show this screen
+  -v --version                  Show version
+  --format <fmt>                Report format [default: pretty]
+
+DOCOPT;
+
+    $result = Docopt::handle($doc, array('version' => '0.0.1'));
+    $diff = genDiff($result->args["<firstFile>"], $result->args["<secondFile>"], $result->args["--format"]);
+    echo $diff;
+    echo "\n";
+}
